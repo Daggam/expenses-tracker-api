6a24d38b4bc5bb18b64cab6232a9b7fbbd00d131
Benjamin Villa <daggamarg@gmail.com>
2025-12-07 20:18:19 -0300

    feat(alembic)!: database migration added.
    BREAKING CHANGES!
    * Expenses categories created.

---
+++ alembic/versions/ba8ca2de27a9_create_categories.py
@@ -0,0 +1,36 @@
+"""create categories
+
+Revision ID: ba8ca2de27a9
+Revises: c11a09ed5954
+Create Date: 2025-12-07 19:56:00.242954
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'ba8ca2de27a9'
+down_revision: Union[str, Sequence[str], None] = 'c11a09ed5954'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    meta = sa.MetaData()
+    expense_category = sa.Table('expense_category',meta,autoload_with=op.get_bind())
+    op.bulk_insert(expense_category,
+                   [ {"category":e} for e in ["Groceries","Leisure","Electronics","Clothing","Health","Others"]]
+                   )
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    meta = sa.MetaData()
+    expense_category = sa.Table('expense_category',meta,autoload_with=op.get_bind())
+    op.drop_table(expense_category)
+    op.create_table('expense_category',expense_category)
+    pass
