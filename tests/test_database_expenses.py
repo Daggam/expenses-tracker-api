aa6829a231388aa15cf0b12c2067331295ce8cc1
Benjamin Villa <daggamarg@gmail.com>
2025-12-07 19:23:42 -0300

    test: database tests added/modified.

--- tests/test_database_expenses.py
+++ tests/test_database_expenses.py
@@ -1,25 +1,37 @@
 from src.models.expenses import Expense,ExpenseCategory
+from src.models.users import User
 import datetime
 import pytest
 
 from tests.conftest import Session
 
-# @pytest.fixture
-# def add_categpru(db_session:Session):
-#     expense = Expense(1,"Hola")
-#     db_session.add(expense)
-#     db_session.commit()
-#     return expense
-    
+@pytest.fixture
+def add_user(db_session:Session):
+    user = User(username='daggam',email='benjaminvilla409@gmail.com',password_hash='adfjd')
+    db_session.add(user)
+    return user
 
-def test_add_expense(db_session:Session):
-    category = ExpenseCategory(id=1,category="Prueba")
-    category.expenses.append(Expense(id=1,name='Expensa 1'))
-    category.expenses.append(Expense(id=2,name='Expensa 2'))
-    category.expenses.append(Expense(id=3,name='Expensa 3'))
+def test_add_expense(db_session:Session,add_user):
+    #Primero se crea la categoria a la cual pertenecen las expensas
+    category = ExpenseCategory(category="Prueba")
+    my_expenses = [Expense(name='Expensa 1'),
+                   Expense(name='Expensa 2'),
+                   Expense(name='Expensa 3')
+                   ]
+    #Luego el usuario crea las expensas (Las expensas dependen de la categoria y del usuario)
+    add_user.expenses = my_expenses
+    category.expenses = my_expenses
 
     db_session.add(category)
     db_session.commit()
     assert category.category == "Prueba"
     assert len(category.expenses) == 3
     assert category.expenses[0].name == 'Expensa 1'
+
+def test_add_user(db_session:Session):
+    user = User(username='Pruebin',email='pruebin@gmail.com',password_hash='adfjd')
+    db_session.add(user)
+    db_session.commit()
+    assert user.username == 'Pruebin'
+    assert len(user.expenses) == 0
+
